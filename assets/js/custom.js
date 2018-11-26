3ef646b9d55725e8315c4b0f9c29f9d9408beae5
broliux <abramikas@gmail.com>
2018-11-26 17:10:24 +0200

    17:10 11-26

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -21,5 +21,32 @@ window.onload = function() {
 	    document.getElementById("menu").style.top = "-"+h+"px";
 	  }
 	  prevScrollpos = currentScrollPos;
-	} 
+	}
 };
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slide");
+  var dots = document.getElementsByClassName("dot");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" dot_current", "");
+  }
+  x[slideIndex-1].style.display = "";  
+  dots[slideIndex-1].className += " dot_current";
+}
